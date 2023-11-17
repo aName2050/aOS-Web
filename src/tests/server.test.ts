94e7fe8264188de34997196b017a77bca52b80c0
aName <shafaeebardia@gmail.com>
2023-11-17 17:24:14 +0000

    removed logs

--- src/tests/server.test.ts
+++ src/tests/server.test.ts
@@ -1,22 +1,20 @@
-import request from 'supertest';
+import request from "supertest";
 
-import { app } from '../Server/server';
+import { app } from "../Server/server";
 
-describe('Server, APIs, and Middleware Tests', () => {
-    test('OS API', async () => {
-        const res = await request(app).get('/client/os/system/api/ping');
-        expect(res.body).toEqual({ message: 'Pong!' });
-    });
+describe("Server, APIs, and Middleware Tests", () => {
+	test("OS API", async () => {
+		const res = await request(app).get("/client/os/system/api/ping");
+		expect(res.body).toEqual({ message: "Pong!" });
+	});
 
-    test('Auto Language Detection (no lang code in URL)', async () => {
-        const res: request.Response = await request(app).get('/');
-        console.log(res.status, res.text);
-        expect(res.status).toBe(302);
-    });
+	test("Auto Language Detection (no lang code in URL)", async () => {
+		const res: request.Response = await request(app).get("/");
+		expect(res.status).toBe(302);
+	});
 
-    test('Auto Language Detection (lang code in URL)', async () => {
-        const res: request.Response = await request(app).get('/en');
-        console.log(res.status, res.text);
-        expect(res.status).toBe(200);
-    });
+	test("Auto Language Detection (lang code in URL)", async () => {
+		const res: request.Response = await request(app).get("/en");
+		expect(res.status).toBe(200);
+	});
 });
