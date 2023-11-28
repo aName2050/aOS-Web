ab2c760bcf666761382c3695c591776074749d4e
aName <shafaeebardia@gmail.com>
2023-11-28 18:24:21 +0000

    worked on making navbar responsive

--- src/Client/JS/home.js
+++ src/Client/JS/home.js
@@ -1,16 +1,23 @@
 window.onload = async () => {
-    // Reload page once to resolve any network issues
-    var reloaded = sessionStorage.getItem('reloaded');
-    if (reloaded == 'false' || !reloaded) {
-        sessionStorage.setItem('reloaded', 'true');
-        location.reload();
-    }
+	// Reload page once to resolve any network issues
+	var reloaded = sessionStorage.getItem("reloaded");
+	if (reloaded == "false" || !reloaded) {
+		sessionStorage.setItem("reloaded", "true");
+		location.reload();
+	}
 
-    // Load buttons
-    var navButtons = document.getElementById('navButtons');
+	// Load buttons
+	var navButtons = document.getElementById("navButtons");
 
-    var userStatusRaw = await fetch('/server/api/user/account/status');
-    var userStatus = await userStatusRaw.json();
+	var userStatusRaw = await fetch("/server/api/user/account/status");
+	var userStatus = await userStatusRaw.json();
 
-    console.log(userStatus);
+	console.log(userStatus);
 };
+
+window.addEventListener("scroll", function () {
+	var elementTarget = document.getElementById("mainHeader");
+	if (window.scrollY > elementTarget.offsetTop + elementTarget.offsetHeight) {
+		alert("You've scrolled past the second div");
+	}
+});
