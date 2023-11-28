61419496851a1a3a1d3125188844d72bf14727b6
aName2050 <shafaeebardia@gmail.com>
2023-11-27 23:47:02 -0800

    added comment

--- src/tests/server.test.ts
+++ src/tests/server.test.ts
@@ -1,20 +1,21 @@
-import request from "supertest";
+import request from 'supertest';
 
-import { app } from "../Server/server";
+import { app } from '../Server/server';
 
-describe("Server, APIs, and Middleware Tests", () => {
-	test("OS API", async () => {
-		const res = await request(app).get("/client/os/system/api/ping");
-		expect(res.body).toEqual({ message: "Pong!" });
-	});
+// UNIT TESTS: Server side handling
+describe('Server, APIs, and Middleware Tests', () => {
+    test('OS API', async () => {
+        const res = await request(app).get('/client/os/system/api/ping');
+        expect(res.body).toEqual({ message: 'Pong!' });
+    });
 
-	test("Auto Language Detection (no lang code in URL)", async () => {
-		const res: request.Response = await request(app).get("/");
-		expect(res.status).toBe(302);
-	});
+    test('Auto Language Detection (no lang code in URL)', async () => {
+        const res: request.Response = await request(app).get('/');
+        expect(res.status).toBe(302);
+    });
 
-	test("Auto Language Detection (lang code in URL)", async () => {
-		const res: request.Response = await request(app).get("/en");
-		expect(res.status).toBe(200);
-	});
+    test('Auto Language Detection (lang code in URL)', async () => {
+        const res: request.Response = await request(app).get('/en');
+        expect(res.status).toBe(200);
+    });
 });
