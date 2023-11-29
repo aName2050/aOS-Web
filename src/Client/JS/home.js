2d03529657e07946d668100eed75c7207099ffb4
aName2050 <shafaeebardia@gmail.com>
2023-11-28 22:22:47 -0800

    finished main navbar elements

--- src/Client/JS/home.js
+++ src/Client/JS/home.js
@@ -1,24 +1,28 @@
 window.onload = async () => {
-	// Reload page once to resolve any network issues
-	var reloaded = sessionStorage.getItem("reloaded");
-	if (reloaded == "false" || !reloaded) {
-		sessionStorage.setItem("reloaded", "true");
-		location.reload();
-	}
+    // Reload page once to resolve any network issues
+    var reloaded = sessionStorage.getItem('reloaded');
+    if (reloaded == 'false' || !reloaded) {
+        sessionStorage.setItem('reloaded', 'true');
+        location.reload();
+    }
 
-	// Load buttons
-	// var navButtons = document.getElementById("navButtons");
+    // Load buttons
+    // var navButtons = document.getElementById("navButtons");
 
-	var userStatusRaw = await fetch("/server/api/user/account/status");
-	var userStatus = await userStatusRaw.json();
+    var userStatusRaw = await fetch('/server/api/user/account/status');
+    var userStatus = await userStatusRaw.json();
 
-	console.log(userStatus);
+    console.log(userStatus);
 };
 
-window.addEventListener("scroll", function () {
-	var header = document.getElementById("mainHeader");
-	if (window.scrollY > header.offsetTop + header.offsetHeight) {
-		var navbar = document.getElementById("navbar");
-		navbar.classList.add("navbar-shadow");
-	}
+// Navbar box shadow only appears when scrolling far enough down
+// the page to cover an element
+window.addEventListener('scroll', function () {
+    var header = document.getElementById('mainHeader');
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY + header.style.height > header.offsetHeight) {
+        navbar.classList.add('navbar-shadow');
+    } else {
+        navbar.classList.remove('navbar-shadow');
+    }
 });
