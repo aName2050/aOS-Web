c6ec70c209ecddd2777f819b0f8e0a05b8cc2fd7
aName2050 <shafaeebardia@gmail.com>
2023-11-30 22:53:12 -0800

    finished footer, worked on body

--- src/Client/JS/home.js
+++ src/Client/JS/home.js
@@ -14,15 +14,3 @@ window.onload = async () => {
 
     console.log(userStatus);
 };
-
-// Navbar box shadow only appears when scrolling far enough down
-// the page to cover an element
-window.addEventListener('scroll', function () {
-    var header = document.getElementById('mainHeader');
-    var navbar = document.getElementById('navbar');
-    if (window.scrollY + header.style.height > header.offsetHeight) {
-        navbar.classList.add('navbar-shadow');
-    } else {
-        navbar.classList.remove('navbar-shadow');
-    }
-});
---
+++ src/Client/JS/styles.js
@@ -0,0 +1,11 @@
+// Navbar box shadow only appears when scrolling far enough down
+// the page to cover an element
+window.addEventListener('scroll', function () {
+    var header = document.getElementById('main');
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY + header.style.height > header.offsetHeight) {
+        navbar.classList.add('navbar-shadow');
+    } else {
+        navbar.classList.remove('navbar-shadow');
+    }
+});
