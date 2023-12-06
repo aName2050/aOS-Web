876000b7173f848bd868f8f564d14bdf2abdb734
aName <shafaeebardia@gmail.com>
2023-12-06 19:33:56 +0000

    finish site integrity notification

--- src/Client/JS/system-integrity.js
+++ src/Client/JS/system-integrity.js
@@ -1,34 +1,38 @@
 const showNotification = () => {
-    const notification = document.getElementById('customNotification');
-    if (notification) {
-        notification.style.display = 'block';
-    }
+	const notification = document.getElementsByClassName(
+		"securityNotification"
+	)[0];
+	if (notification) {
+		notification.style.display = "block";
+	}
 };
 
 const hideNotification = () => {
-    const notification = document.getElementById('customNotification');
-    if (notification) {
-        notification.style.display = 'none';
-    }
+	const notification = document.getElementsByClassName(
+		"securityNotification"
+	)[0];
+	if (notification) {
+		notification.style.display = "none";
+	}
 };
 
 const observerCallback = (mutationsList, observer) => {
-    mutationsList.forEach(mutation => {
-        mutation.addedNodes.forEach(node => {
-            if (node.nodeType === 1) {
-                // Check if the injected element is intentional
-                const isIntentional = node.classList.contains(
-                    'self-html-injection'
-                ); // Use a class or other attribute you intentionally set
+	mutationsList.forEach((mutation) => {
+		mutation.addedNodes.forEach((node) => {
+			if (node.nodeType === 1) {
+				// Check if the injected element is intentional
+				const isIntentional = node.classList.contains(
+					"self-html-injection"
+				); // Use a class or other attribute you intentionally set
 
-                if (!isIntentional) {
-                    // It's not intentional, remove it and show the on-site notification
-                    node.parentNode.removeChild(node);
-                    showNotification();
-                }
-            }
-        });
-    });
+				if (!isIntentional) {
+					// It's not intentional, remove it and show the on-site notification
+					node.parentNode.removeChild(node);
+					showNotification();
+				}
+			}
+		});
+	});
 };
 
 // Create a new observer with the callback
